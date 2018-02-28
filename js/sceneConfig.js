3306b757fc7f9ed69d5d3ecce5a7468d6f32d56a
DoctorTone <drtonesoftware@gmail.com>
2018-02-28 23:14:48 +0000

    Initial commit.

---
+++ js/sceneConfig.js
@@ -0,0 +1,37 @@
+/**
+ * Created by DrTone on 16/07/2017.
+ */
+//All settings related to the scene
+
+const CENTRE_HEIGHT = 105;
+const CENTRE_RADIUS = 5;
+const SEGMENTS = 16;
+const WALL_HEIGHT = 100;
+const WALL_DEPTH = 120;
+const WALL_WIDTH = 5;
+const NUM_WALLS_DAILY = 5;
+const NUM_WALLS_WEEKLY = 6;
+const MONTHLY_START = 400;
+const COLUMN_HEIGHT = 1;
+const COLUMN_RADIUS = 3;
+const NUM_SEGMENTS = 5;
+const NUM_COLUMNS_PER_SEGMENT = 5;
+const WALL_OPACITY = 0.55;
+const CLOSE_PRICE = 2;
+const MONTHLY_X = 250;
+const MONTHLY_Y = 0;
+const MONTHLY_Z = 0;
+const WEEKLY_X = -250;
+const WEEKLY_Y = 0;
+const WEEKLY_Z = 0;
+const DISPLAY_MONTHS = 5;
+const MOVE_SPEED = 20;
+const CAM_POS_NEAR_X = 0;
+const CAM_POS_NEAR_Y = 110;
+const CAM_POS_NEAR_Z = 280;
+const CAM_POS_FAR_X = -250;
+const CAM_POS_FAR_Y = 160;
+const CAM_POS_FAR_Z = 500;
+const NEAR = 0;
+const FAR = 1;
+const MOBILE_WIDTH = 768;
